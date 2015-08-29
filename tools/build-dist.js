79a039169c39c3bbd8c3cb5e23146b847e375a5f
Andre Pena <andrerpena@gmail.com>
2015-08-29 10:36:47 -0300

    Dumping Gulp. I'm going to use NPM instead

---
+++ tools/build-dist.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var rimraf = require('rimraf-promise');
+var colors = require('colors');
+var exec = require('child-process-promise').exec;
+
+console.log('Building dist'.green);
+rimraf('./dist').then(function (error) {
+    var webpackCli = 'webpack --config webpack.config.dist.js';
+    return exec(webpackCli).fail(function (error) {
+        console.log(colors.red(error))
+    });
+}).then(() => console.log('dist built'.green));
+
---
+++ tools/build-lib.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var rimraf = require('rimraf-promise');
+var colors = require('colors');
+var exec = require('child-process-promise').exec;
+
+console.log('Building lib'.green);
+
+rimraf('./lib')
+    .then(function (error) {
+        let babelCli = 'babel --optional es7.objectRestSpread ./src --out-dir ./lib';
+        return exec(babelCli).fail(function (error) {
+            console.log(colors.red(error))
+        });
+    }).then(() => console.log('lib built'.green));
