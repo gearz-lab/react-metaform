fa99488fa128ed8cf68b3f81b133fbc1109d2370
André Pena <andrerpena@gmail.com>
2015-09-23 19:43:16 -0300

    CollectionContainer now is working :D

--- src/components/editors/CollectionContainer.js
+++ src/components/editors/CollectionContainer.js
@@ -22,15 +22,28 @@ const CollectionContainer = React.createClass({
         name: React.PropTypes.string.isRequired
     },
 
+    handleAdd: function() {
+        if(this.props.onChange) {
+            let value = this.props.value;
+            value.push({});
+            this.props.onChange({id: this.props.id, value: value});
+        }
+    },
+
     render: function () {
+
+        let components = this.props.fields.map(f => {
+            return <CollectionContainerItem>
+                    <MetaFormGroup layout={this.props.layout} fields={f}/>
+                </CollectionContainerItem>;
+        });
+
         return (
             <div>
-                <CollectionContainerItem>
-
-                </CollectionContainerItem>
+                {components}
                 <div className="">
                     <span className="pull-right">
-                        <GlyphButton glyph="plus" text="Add" />
+                        <GlyphButton glyph="plus" text="Add" onClick={this.handleAdd} />
                     </span>
                 </div>
             </div>
