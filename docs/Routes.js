a94de4cc4edd5bda4fce601fd9e5760280a76359
André Pena <andrerpena@gmail.com>
2015-09-24 07:27:47 -0300

    Preparing to publish the demo

--- docs/Routes.js
+++ docs/Routes.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 import Root from './pages/Root';
 import HomePage from './pages/HomePage.js';
-import LiveSchemaEditor from './pages/LiveSchemaEditor.js';
+import Demo from './pages/Demo.js';
 
 import {Route, DefaultRoute, NotFoundRoute} from 'react-router';
 
 export default (
     <Route name='app' path='/' handler={Root}>
-        <Route name='home' path='home.html' handler={HomePage}/>
-        <Route name='liveSchemaEditor' path='liveSchemaEditor.html' handler={LiveSchemaEditor}/>
-        <DefaultRoute handler={HomePage}/>
+        <Route name='demo' path='demo.html' handler={Demo}/>
     </Route>
 )
