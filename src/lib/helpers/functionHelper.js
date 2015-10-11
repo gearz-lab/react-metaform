68615adcb5462143a44fd19422f027bf3d289224
André Pena <andrerpena@gmail.com>
2015-10-11 00:03:59 -0300

    MInor improvements in the FunctionHelper

--- src/lib/helpers/functionHelper.js
+++ src/lib/helpers/functionHelper.js
@@ -8,8 +8,17 @@ class FunctionHelper {
      * Returns whether or not the given string is a text expression
      * @param textExpression
      */
-    isExpression(textExpression) {
-        return this.regex.test(textExpression);
+    isTextExpression(textExpression) {
+        return typeof textExpression === 'string' && this.regex.test(textExpression);
+    }
+
+    /**]
+     * Returns whether or not the given variable is a function
+     * @param func
+     * @returns {boolean}
+     */
+    isFunction(func) {
+        return typeof func === 'function';
     }
 
     /**
@@ -29,6 +38,21 @@ class FunctionHelper {
             // in case the function is not valid it should just return null;
             return null;
         }
+    }
+
+    /**
+     * If the object passed in is a function, returns the function. If it's a text expression, returns the converted
+     * function. Otherwise, throws an exception
+     * @param func
+     */
+    getFunction(func) {
+        if(this.isFunction(func)) {
+            return func;
+        }
+        else if (this.isTextExpression(func)) {
+            return this.generateFromTextExpression(func);
+        }
+        throw Error('func should be either a function or a text expression');
     }
 }
 
