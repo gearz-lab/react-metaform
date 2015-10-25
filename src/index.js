23b88ad84674b51c108ef02737dcb8a9443f2a82
andrerpena@gmail.com <andrerpena@gmail.com>
2015-10-25 17:00:58 -0200

    Bug fix: The Webpack entry point, index.js, was not including the styles.less file, making the build-dist tool not to export styles I'm no longer using Bootstrap variables in the Less files so Bootstrap will not automatically be required in the bundle.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import MetaForm from './MetaForm.js';
 import ComponentFactory from './ComponentFactory.js';
 import DefaultComponentFactory from './DefaultComponentFactory.js';
+import styles from './less/styles.less';
 
 export default {
     MetaForm,
