de91e6547a7cab4da97911ab6d9acf2ad8ddf10a
andrerpena <andrerpena@gmail.com>
2015-11-21 18:56:57 -0200

    Cleaning out the demo's Router.js file

--- demo/Routes.js
+++ demo/Routes.js
@@ -5,12 +5,6 @@ import Demo from './pages/Demo.js';
 
 import {Route} from 'react-router';
 
-var DefaultComponent = React.createClass({
-    render: function() {
-        return <div>Please go to /react-metaform/demo.html</div>;
-    }
-});
-
 export default (
     <Route path='/' component={Root}>
         <Route path='/react-metaform/demo.html' component={Demo} />
